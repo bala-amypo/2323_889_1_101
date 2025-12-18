d5b5403329176eb301dcb853cbbcdd8a9fab42a3
bala-amypo <bala@amypo.in>
2025-12-18 10:04:55 +0000

    User commit

--- src/main/java/com/example/demo/repository/ConsumptionLogRepository.java
+++ src/main/java/com/example/demo/repository/ConsumptionLogRepository.java
@@ -1,2 +1,5 @@
 package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+public interface ConsumptionLogRepository extends JpaRepository<ConsumptionLog,Long>{
+    
+}

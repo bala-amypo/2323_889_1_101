fb423116df7e84682e9f253d788d179dc001aba4
bala-amypo <bala@amypo.in>
2025-12-18 08:35:26 +0000

    User commit

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -2,4 +2,10 @@ package com.example.demo.model;
 
 public class Product{
     
+    private long id;
+    private  String productName;
+    @coloumn(uni)
+    private String sku;
+    private String category;
+    private LocalDateTime createdAt;
 }

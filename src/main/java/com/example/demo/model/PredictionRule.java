6cca3226dede3d3cd2934dafc7b0da16d57c41b3
bala-amypo <bala@amypo.in>
2025-12-18 09:19:55 +0000

    User commit

--- src/main/java/com/example/demo/model/PredictionRule.java
+++ src/main/java/com/example/demo/model/PredictionRule.java
@@ -2,6 +2,7 @@ package com.example.demo.model;
 
 public class PredictionRule{
     private Long id;
+    @coloumn(unique=true)
     private String ruleName;
     @Min(1)
     private Integer averageDaysWindow;

a7e74a50a9c2e2f552aee12285485c9aedcf7827
bala-amypo <bala@amypo.in>
2025-12-18 08:49:58 +0000

    User commit

--- src/main/java/com/example/demo/model/StockRecord.java
+++ src/main/java/com/example/demo/model/StockRecord.java
@@ -2,6 +2,11 @@ package com.example.demo.model;
 
 public class StockRecord{
     private Long id;
-    private 
+    @coloumn(unique=true)
+    private String warehouseName;
+    @NotBlank="should not be empty"
+    private Integer
+    private  Integer reorderThreshold;
+    private LocalDateTime lastUpdated;
     
 }

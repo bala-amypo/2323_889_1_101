43efd7e6974d16543c5319160f03ce2d55365fe7
bala-amypo <bala@amypo.in>
2025-12-18 09:04:57 +0000

    User commit

--- src/main/java/com/example/demo/model/ConsumptionLog.java
+++ src/main/java/com/example/demo/model/ConsumptionLog.java
@@ -1,5 +1,10 @@
 package com.example.demo.model;
 
 public class ConsumptionLog{
+    private Long id;
+    private stockRecord;
+    private Integer  Integer consumedQuantity;
+    private Integer reorderThreshold;
+    private LocalDateTime lastUpdated;
     
 }

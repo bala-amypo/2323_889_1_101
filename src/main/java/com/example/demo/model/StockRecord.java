5ce76032b8f53fb03691a9bdfa32dd863b4a2bf6
bala-amypo <bala@amypo.in>
2025-12-18 08:59:57 +0000

    User commit

--- src/main/java/com/example/demo/model/StockRecord.java
+++ src/main/java/com/example/demo/model/StockRecord.java
@@ -1,13 +1,16 @@
 package com.example.demo.model;
 import jakarta.persistence.Id;
-import jakarta.
+import jakarta.validation.constraints.Min;
+
 
 public class StockRecord{
     private Long id;
     private product;
+    @coloumn(unique=true)
     private  warehouse;
     @Min(0)
     private Integer currentQuantity;
+    @Min(1)
     private  Integer reorderThreshold;
     private LocalDateTime lastUpdated;
     

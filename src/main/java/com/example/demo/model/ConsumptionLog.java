50bed09d98a5740993f98100f9003b3cf9116d24
bala-amypo <bala@amypo.in>
2025-12-19 04:32:18 +0000

    User commit

--- src/main/java/com/example/demo/model/ConsumptionLog.java
+++ src/main/java/com/example/demo/model/ConsumptionLog.java
@@ -9,29 +9,7 @@ public class ConsumptionLog{
     private Integer consumedQuantity;
     private Integer reorderThreshold;
     private LocalDateTime lastUpdated;
-     public StudentEntity(long id, @NotBlank(message = "should not contain spaces") String name,   @NotBlank(message = "no blank allowed") String email) {
-      this.id = id;
-      this.name = name;
-      this.email = email;
-    }
-    public long getId() {
-       return id;
-    }
-    public void setId(long id) {
-       this.id = id;
-    }
-    public String getName() {
-       return name;
-    }
-    public void setName(String name) {
-       this.name = name;
-    }
-    public String getEmail() {
-       return email;
-    }
-    public void setEmail(String email) {
-       this.email = email;
-    }
+    
      
     
 }

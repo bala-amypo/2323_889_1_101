9453b15b973e7e6f766fd881db69f5ffd0961860
bala-amypo <bala@amypo.in>
2025-12-21 17:13:34 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/StockRecordController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.StockRecord;
+import com.example.demo.service.StockRecordService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/stocks")
+public class StockRecordController {
+
+    private final StockRecordService stockRecordService;
+
+    public StockRecordController(StockRecordService stockRecordService) {
+        this.stockRecordService = stockRecordService;
+    }
+
+    @PostMapping("/{productId}/{warehouseId}")
+    public StockRecord createStockRecord(
+            @PathVariable String productId,
+            @PathVariable String warehouseId,
+            @RequestBody StockRecord record) {
+
+        return stockRecordService.createStockRecord(productId, warehouseId, record);
+    }
+
+    @GetMapping("/{id}")
+    public StockRecord getStockRecord(@PathVariable String id) {
+        return stockRecordService.getStockRecord(id);
+    }
+
+    @GetMapping("/product/{productId}")
+    public List<StockRecord> getByProduct(@PathVariable String productId) {
+        return stockRecordService.getRecordsBy_product(productId);
+    }
+
+    @GetMapping("/warehouse/{warehouseId}")
+    public List<StockRecord> getByWarehouse(@PathVariable String warehouseId) {
+        return stockRecordService.getRecordsByWarehouse(warehouseId);
+    }
+}
---
+++ src/main/java/com/example/demo/controller/WarehouseController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Warehouse;
+import com.example.demo.service.WarehouseService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/warehouses")
+public class WarehouseController {
+
+    private final WarehouseService warehouseService;
+
+    public WarehouseController(WarehouseService warehouseService) {
+        this.warehouseService = warehouseService;
+    }
+
+    @PostMapping
+    public Warehouse createWarehouse(@RequestBody Warehouse warehouse) {
+        return warehouseService.createWarehouse(warehouse);
+    }
+
+    @GetMapping
+    public List<Warehouse> getAllWarehouses() {
+        return warehouseService.getAllWarehouses();
+    }
+
+    @GetMapping("/{id}")
+    public Warehouse getWarehouse(@PathVariable String id) {
+        return warehouseService.getWarehouse(id);
+    }
+}

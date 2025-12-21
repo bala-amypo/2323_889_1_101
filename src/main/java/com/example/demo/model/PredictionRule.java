08f70c96e709eada33d5896529cd2f4577969e85
bala-amypo <bala@amypo.in>
2025-12-21 17:28:34 +0000

    User commit

--- src/main/java/com/example/demo/model/PredictionRule.java
+++ src/main/java/com/example/demo/model/PredictionRule.java
@@ -1,17 +1,31 @@
 package com.example.demo.model;
-import jakarta.persistence.Id;
-import jakarta.persistence.Coloumn;
 
-import jakarta.validation.constraints.Min;
+import jakarta.persistence.*;
+import lombok.*;
 
-public class PredictionRule{
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "prediction_rules", uniqueConstraints = @UniqueConstraint(columnNames = "ruleName"))
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PredictionRule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Coloumn(unique=true)
+
+    @Column(nullable = false, unique = true)
     private String ruleName;
-    @Min(1)
+
     private Integer averageDaysWindow;
+
     private Integer minDailyUsage;
+
     private Integer maxDailyUsage;
+
     private LocalDateTime createdAt;
-    
 }
--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -1,13 +1,30 @@
 package com.example.demo.model;
-import jakarta.persistence.Id;
-import jakarta.persistence.Coloumn;
-public class Product{
-    
-    private long id;
-    @NotBlank(message="should not be empty")
-    private  String productName;
-    @coloumn(unique=true)
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products", uniqueConstraints = @UniqueConstraint(columnNames = "sku"))
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false, unique = true)
     private String sku;
+
     private String category;
+
     private LocalDateTime createdAt;
 }
